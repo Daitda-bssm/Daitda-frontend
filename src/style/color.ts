2a1b3904566b34adc03e25bb36ffd6c7b15287c5
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-05 23:50:17 +0900

    feat : 프로젝트 색상 추가

---
+++ src/style/color.ts
@@ -0,0 +1,13 @@
+export const color = {
+  gray900: "#1B1D1F",
+  gray800: "#26282B",
+  gray600: "#454C53",
+  gray500: "#72787F",
+  gray400: "#9EA4AA",
+  gray200: "#C9CDD2",
+  gray100: "#E8EBED",
+  gray50: "#F7F8F9",
+  green: "#53E183",
+  white: "#F5F5F5",
+  black: "#222222",
+};
