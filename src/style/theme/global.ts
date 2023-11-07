56db22030923fdd9d04e7ce5acb08fcc27ccbcad
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-07 10:46:51 +0900

    feat : button 전역 스타일 설정 추가

--- src/style/theme/global.ts
+++ src/style/theme/global.ts
@@ -1,14 +1,24 @@
 import { createGlobalStyle } from "styled-components";
+import { C } from "./color";
 
 const GlobalStyle = createGlobalStyle`
 * {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+
+    background-color: ${C.white};
   }
 
   html {
     font-size: 10px;
+  }
+
+  button {
+    outline: none;
+    border: none;
+
+    cursor: pointer;
   }
 
 `;
