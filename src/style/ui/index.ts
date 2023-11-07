bb0c7328d8eea13655c7a8e0e626e37176aca36e
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-07 10:50:40 +0900

    feat : UI 컴포넌트 한번에 export

---
+++ src/style/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as Column } from "@/style/ui/Column";
+export { default as Row } from "@/style/ui/Row";
+export { default as AppLayout } from "@/style/ui/Layouts/AppLayout";
+export { default as Text } from "@/style/ui/Text";
