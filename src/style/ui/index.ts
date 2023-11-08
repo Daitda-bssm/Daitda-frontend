1f1d33bc3d45b080100dcd9c8ce26007d7d3ccd8
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-08 20:37:23 +0900

    fix : Dropdown export 제대로 안되어있던 버그 수정

--- src/style/ui/index.ts
+++ src/style/ui/index.ts
@@ -3,5 +3,5 @@ export { default as Row } from "@/style/ui/Row";
 export { default as AppLayout } from "@/style/ui/Layouts/AppLayout";
 export { default as Text } from "@/style/ui/Text";
 export { default as FlexibleImgContainer } from "@/style/ui/FlexibleImgContainer";
-export { default as DropDown } from "@/style/ui/Dropdown";
+export { default as Dropdown } from "@/style/ui/Dropdown";
 export { default as Radio } from "@/style/ui/Radio";
