5d0069b03e9524d5c277b94ee11a083005714eda
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-13 17:47:19 +0900

    fix : radio태그 기본넓이, display 속성 grid에서 flex로 변경

--- src/style/ui/Radio/index.tsx
+++ src/style/ui/Radio/index.tsx
@@ -26,31 +26,32 @@ const Radio = ({ radioElementList }: RadiotypeAttribute) => {
 export default Radio;
 
 const Container = styled.div`
-  width: 11.1rem;
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  width: fit-content;
+  height: fit-content;
+  display: flex;
+  gap: 0.2rem;
   padding: 0.2rem;
-  box-sizing: border-box;
   border-radius: 0.4rem;
   background-color: ${C.gray50};
 `;
 
 const RadioElement = styled.div<{ selected: boolean }>`
+  width: fit-content;
+  height: fit-content;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0.8rem 0.6rem;
+  padding: 0.7rem 1.2rem;
   ${fonts.SubHead}
 
   cursor: pointer;
   transition: 0.5s cubic-bezier(0.3, 0.49, 0.5, 1);
 
   ${({ selected }) =>
-    selected
-      ? css`
-          box-shadow: 0px 0px 5px ${C.gray75};
-          background-color: ${C.white};
-          border-radius: 0.4rem;
-        `
-      : ""}
+    selected &&
+    css`
+      box-shadow: 0px 0px 5px ${C.gray75};
+      background-color: ${C.white};
+      border-radius: 0.4rem;
+    `}
 `;
