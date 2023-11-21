015c90a11034da0bf650a62ef6ebeabf34545882
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-21 09:56:57 +0900

    feat : ModalLayout 컴포넌트 export 문 추가

--- src/style/ui/index.ts
+++ src/style/ui/index.ts
@@ -1,6 +1,7 @@
 export { default as Column } from "@/style/ui/Column";
 export { default as Row } from "@/style/ui/Row";
 export { default as AppLayout } from "@/style/ui/Layouts/AppLayout";
+export { default as ModalLayout } from "@/style/ui/Layouts/ModalLayout";
 export { default as Text } from "@/style/ui/Text";
 export { default as FlexibleImgContainer } from "@/style/ui/FlexibleImgContainer";
 export { default as Dropdown } from "@/style/ui/Dropdown";
