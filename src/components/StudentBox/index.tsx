235ca94e6a5e3bee50155870935f99ce03b25c87
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-11-23 12:04:25 +0900

    refactor : studentBox 스타일 리뉴얼

--- src/components/StudentBox/index.tsx
+++ src/components/StudentBox/index.tsx
@@ -1,7 +1,7 @@
 import { StudentBoxAttribute } from "@/type/components/StudentBoxAttrubute.type";
 import styled from "styled-components";
 
-import { Row, Text } from "@/style/ui";
+import { Column, Row, Text } from "@/style/ui";
 import { C } from "@/style/theme";
 import { PositionTag } from "@/components";
 
@@ -16,30 +16,32 @@ const StudentBox = ({
   return (
     <Container onClick={onClick}>
       <임시이미지 />
-      <Row gap={1.3} alignItems="center">
-        <Text fontType="Title2">{name}</Text>
-        <Text fontType="Callout" color={C.gray400}>
-          {grade}학년
-        </Text>
-      </Row>
-      <Row alignItems="center" gap={1}>
-        <PositionTag tagName={position} />
-        <FramworkContainer gap={1}>
-          {frameworks.map((e, i) => {
-            return (
-              <>
-                <Text
-                  fontType="Footnote"
-                  color={C.gray400}
-                  key={`Frameworks ${i}`}
-                >
-                  # {e}
-                </Text>
-              </>
-            );
-          })}
-        </FramworkContainer>
-      </Row>
+      <Column gap={0.6}>
+        <Row gap={1.3} alignItems="center">
+          <Text fontType="Title2">{name}</Text>
+          <Text fontType="Callout" color={C.gray400}>
+            {grade}학년
+          </Text>
+        </Row>
+        <Row alignItems="center" gap={1}>
+          <PositionTag tagName={position} />
+          <FramworkContainer gap={1}>
+            {frameworks.map((e, i) => {
+              return (
+                <>
+                  <Text
+                    fontType="Footnote"
+                    color={C.gray400}
+                    key={`Frameworks ${i}`}
+                  >
+                    #{e}
+                  </Text>
+                </>
+              );
+            })}
+          </FramworkContainer>
+        </Row>
+      </Column>
     </Container>
   );
 };
@@ -49,8 +51,8 @@ export default StudentBox;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-
-  gap: 1.4rem;
+  margin: 2.6rem 0;
+  gap: 1.2rem;
 
   padding: 0.5rem;
   box-sizing: border-box;
@@ -63,14 +65,21 @@ const Container = styled.div`
   &:hover {
     background-color: ${C.gray50};
   }
+
+  &:active {
+    transition: 0.05s cubic-bezier(0.3, 0.49, 0.5, 1);
+    background-color: ${C.gray100};
+  }
 `;
 
 const 임시이미지 = styled.div`
-  width: 28rem;
+  width: 31rem;
   height: 20rem;
 
   background-color: black;
   border-radius: 1.2rem;
 `;
 
-const FramworkContainer = styled(Row)``;
+const FramworkContainer = styled(Row)`
+  gap: 1rem;
+`;
