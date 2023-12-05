904ad85435314fc084ba87e81fc3cd3dc3bd7264
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-12-05 23:28:32 +0900

    fix : 드랍다운 컴포넌트 속성 추가

--- src/type/ui/DropdownAttribute.type.ts
+++ src/type/ui/DropdownAttribute.type.ts
@@ -1,4 +1,8 @@
+import { MouseEventHandler } from "react";
+
 export interface DropdownAttribute {
   defaultText: string;
   dropdownList: string[];
+  name: string;
+  onChange: MouseEventHandler<HTMLButtonElement>;
 }
