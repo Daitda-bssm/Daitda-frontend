a8c6a465b4c4dbd7144e3d7d053af96da3b4e91d
rlawnsrud0509 <rlawnsrud0509@gmail.com>
2023-12-05 23:27:04 +0900

    feat : Input 태그 추가

---
+++ src/style/ui/Input/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled, { RuleSet } from "styled-components";
+import { Text } from "@/style/ui";
+import { InputAttribute } from "@/type/ui/InputAttribute.type";
+
+const Input = ({
+  name,
+  width,
+  height,
+  fontType,
+  label,
+  placeholder,
+  maxLength,
+  onChange,
+}: InputAttribute) => {
+  return (
+    <Container>
+      {label && <Text fontType="SubHead">{label}</Text>}
+      <StyledInput
+        fontType={fontType}
+        name={name}
+        style={{ width: `${width}rem`, height: `${height}rem` }}
+        placeholder={placeholder}
+        onChange={onChange}
+        maxLength={maxLength}
+      />
+    </Container>
+  );
+};
+
+export default Input;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const StyledInput = styled.input<{ fontType: RuleSet<object> }>`
+  outline: none;
+  border: none;
+  padding: 0.6rem;
+  ${({ fontType }) => fontType};
+`;
